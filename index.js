98c41abd932ee1eeb91f415317df09d28121ff8f
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2020-05-05 13:54:25 +1000

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const apiPort = 3000
+const apiPort = 5000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
