f9e89effa65351f1d09f3748413e66466c3b4e77
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2020-05-17 13:54:02 +1000

    allow cors for localhost and deployed

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,19 @@ const app = express();
 const port = process.env.PORT;
 
 var allowedOrigins = ['http://localhost:3000', 'https://jjfresh.netlify.app/'];
-let origin = req.headers.origin;
-if (allowedOrigins.indexOf(origin) > -1){
-    app.use(cors({credentials: true, origin: origin}));
-}
+app.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 app.use(cookieParser());
 app.use(express.json());
 
