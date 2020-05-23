45c06ce800ec5fb75a3280ed3652dba217230d5e
emanmacario <emacario1997@hotmail.com>
2020-05-24 01:03:13 +1000

    Scuffed email sending to user on submit order

--- backend/models/order.js
+++ backend/models/order.js
@@ -33,8 +33,16 @@ const orderSchema = new Schema({
 });
 
 
-orderSchema.pre('save', async () => {
+orderSchema.pre('save', function(next) {
+    // When the booking is final, the system should send a confirmation email 
+    // to the customer with the date, time, type and size of box; along with 
+    // the price of the box chosen.
     var order = this;
+
+    console.log(Object.keys(order));
+    console.log("Order email: " + order.email);
+    console.log('Date: ' + order.date);
+    console.log("ID: " + order._id);
 
     var transporter = nodemailer.createTransport({
         service: 'gmail',
@@ -47,14 +55,24 @@ orderSchema.pre('save', async () => {
     var mailOptions = {
         from: process.env.EMAIL,
         to: order.email,
-        subject: 'New order from JJFresh',
-        text: 'hell yeah brother new order from jjfresh'
+        subject: `Your JJFresh Order Confirmation (#${order._id})`,
+        text: `Hey there! Thanks for placing your order with us. By
+               purchasing your fresh produce from JJFresh, you've helped
+               support an Australian company employing hardworking Australians.
+               
+               We're just processing your order, then your products will be
+               delivered to you on the scheduled date.
+               
+               <INSERT ORDER HERE>`
     }
 
-    let info = await transporter.sendMail(mailOptions);
+    transporter.sendMail(mailOptions)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
 
-    console.log('Message sent: %s', info.messageId);
+    //console.log('Message sent: %s', info.messageId);
 
+    next();
 });
 
 
