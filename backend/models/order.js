bc2195c518e986034b31b5378d96f44bd95f1db0
emanmacario <emacario1997@hotmail.com>
2020-05-24 00:05:16 +1000

    Try email on submit order

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,6 +1,10 @@
 // Schema for order
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 const Schema = mongoose.Schema;
+
+require('dotenv').config();
+
 
 const orderSchema = new Schema({
     email: { type: String, required: true },
@@ -27,6 +31,32 @@ const orderSchema = new Schema({
 {
     timestamps: true,
 });
+
+
+orderSchema.pre('save', async () => {
+    var order = this;
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    var mailOptions = {
+        from: process.env.EMAIL,
+        to: order.email,
+        subject: 'New order from JJFresh',
+        text: 'hell yeah brother new order from jjfresh'
+    }
+
+    let info = await transporter.sendMail(mailOptions);
+
+    console.log('Message sent: %s', info.messageId);
+
+});
+
 
 const Order = mongoose.model('Order', orderSchema);
 module.exports = Order;
