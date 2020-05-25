7aa4d2b215493cd41e5088353575b97d0d500687
btebbutt <55270549+btebbutt@users.noreply.github.com>
2020-05-25 10:58:52 +1000

    Link style.css for home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Homepage from './../public/Homepage.JPG';
+import styles from "./styles.css";
+
 export default class Home extends Component {
 
     render() {
         return (
             <div>
                 <h3>Home</h3>
-                <img src={Homepage}></img>
+                <img src={Homepage} alt={"JJ Fresh Home Page"}/>
                 <a href="/viewBookings" className="btn btn-primary" id="viewOrderButton">View all orders</a>
             </div>
         );
