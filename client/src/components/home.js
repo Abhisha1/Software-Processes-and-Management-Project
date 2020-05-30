3e37f77ac8a448df35a1530179e9dfa4b1b6f7e8
Abhisha Nirmalathas <a.nirmalathas1@student.unimelb.edu.au>
2020-05-30 15:33:13 +1000

    homepage pic

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import Homepage from './../public/Homepage.JPG';
-import styles from "./styles.css";
+import styles from "./Stylesheet.css";
 
 export default class Home extends Component {
 
     render() {
         return (
-            <div class = "pic">
-                <div class = "headings">
-                    <h1 id = "firstHeading" >Welcome To JJ Fresh. Home of the freshest produce you can order. </h1>
-                    <h3 id = "secondHeading"> At JJ fresh we sell only the freshest ingredients, so you can focus on cooking.</h3>
+            <div className="pic">
+                <div className="headings">
+                    <h1 id="firstHeading" >Welcome To JJ Fresh. Home of the freshest produce you can order. </h1>
+                    <h3 id="secondHeading"> At JJ fresh we sell only the freshest ingredients, so you can focus on cooking.</h3>
                     <a href="/viewBookings" className="btn btn-primary" id="viewOrderButton">View all orders</a>
                 </div>
             </div>
