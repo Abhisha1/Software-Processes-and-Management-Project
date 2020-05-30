99016a97d5f9387151da9d3dfb92b6d74fbe7b12
btebbutt <55270549+btebbutt@users.noreply.github.com>
2020-05-30 13:44:00 +1000

    Home page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Homepage from './../public/Homepage.JPG';
+import styles from "./styles.css";
+
 export default class Home extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Home</h3>
-                <img src={Homepage}></img>
-                <a href="/viewBookings" className="btn btn-primary" id="viewOrderButton">View all orders</a>
+            <div class = "pic">
+                <div class = "headings">
+                    <h1 id = "firstHeading" >Welcome To JJ Fresh. Home of the freshest produce you can order. </h1>
+                    <h3 id = "secondHeading"> At JJ fresh we sell only the freshest ingredients, so you can focus on cooking.</h3>
+                    <a href="/viewBookings" className="btn btn-primary" id="viewOrderButton">View all orders</a>
+                </div>
             </div>
+
+
         );
     }
 }
+
+
+
