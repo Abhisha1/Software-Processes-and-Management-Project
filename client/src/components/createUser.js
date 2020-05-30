e457d1ff57520dd7b73746de3f0967560202f0d7
btebbutt <55270549+btebbutt@users.noreply.github.com>
2020-05-30 13:42:36 +1000

    Home page

--- client/src/components/createUser.js
+++ client/src/components/createUser.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng
+} from "react-places-autocomplete";
+//
+// import "https://maps.googleapis.com/maps/api/js?key=AIzaSyAhgPS9xVvesdLc8ETGdv8u31VpKZZDCmA&libraries=places&callback=initMap";
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+}
 class CreateUser extends Component {
+
     constructor(props) {
         super(props);
-
+        // this.autocompleteInput = React.createRef();
+        // this.autocomplete = null;
+        // this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangeMobile = this.onChangeMobile.bind(this);
         this.onChangeHome = this.onChangeHome.bind(this);
         this.onChangeWork = this.onChangeWork.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
-
+        // this.handleSelect = this.handleSelect.bind(this);
+        this.onChangeAddress = this.onChangeAddress.bind(this);
         this.state = {
             email: '',
             password: '',
@@ -26,10 +37,33 @@ class CreateUser extends Component {
             showMobileError: false,
             mobile : '',
             home : '',
-            work : ''
+            work : '',
+            address : '',
+
         }
     }
+    /* componentDidMount() {
+         this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.current,
+             {"types": ["geocode"]});
 
+         this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
+     }
+     handlePlaceChanged(){
+         const place = this.autocomplete.getPlace();
+         this.props.onPlaceLoaded(place);
+     }*/
+    /*handleSelect = async value => {
+        const results = await geocodeByAddress(value);
+        const latLng = await getLatLng(results[0]);
+        this.state.setAddress = value;
+        this.state.setCoordinates = latLng;
+    };*/
+    onChangeAddress(e){
+        this.setState({
+            address: e.description,
+            showError: false
+        });
+    }
     onChangeEmail(e) {
         this.setState({
             email: e.target.value,
@@ -67,7 +101,8 @@ class CreateUser extends Component {
             password: this.state.password,
             mobile: this.state.mobile,
             home: this.state.home,
-            work: this.state.work
+            work: this.state.work,
+            address: this.state.address
         };
         console.log(user);
 
@@ -107,23 +142,23 @@ class CreateUser extends Component {
                     <div className="form-group">
                         <label>Email address</label>
                         <input type="email"
-                            id="email"
-                            required
-                            className="form-control"
-                            value={this.state.email}
-                            onChange={this.onChangeEmail}
-                            placeholder="Email"
+                               id="email"
+                               required
+                               className="form-control"
+                               value={this.state.email}
+                               onChange={this.onChangeEmail}
+                               placeholder="Email"
                         />
                     </div>
                     <div className="form-group">
                         <label>Password</label>
                         <input type="password"
-                            id="password"
-                            required
-                            className="form-control"
-                            value={this.state.password}
-                            onChange={this.onChangePassword}
-                            placeholder="Password"
+                               id="password"
+                               required
+                               className="form-control"
+                               value={this.state.password}
+                               onChange={this.onChangePassword}
+                               placeholder="Password"
 
                         />
 
@@ -159,15 +194,20 @@ class CreateUser extends Component {
                                placeholder="Work Number"
                         />
                     </div>
+                    <div>
+                        <GooglePlacesAutocomplete apiKey='AIzaSyAhgPS9xVvesdLc8ETGdv8u31VpKZZDCmA'
+                                                  onSelect={this.onChangeAddress}
+                        />
+                    </div>
                     <div className="form-group">
                         <input type="submit" id="submit" value="Sign up" className="btn btn-primary"/>
                     </div>
                     {this.state.showError ?
                         <div id="error" className="alert alert-danger" role="alert" >
-                        The details you entered are invalid, please try again
-                    </div>
-                    :
-                    <div></div>}
+                            The details you entered are invalid, please try again
+                        </div>
+                        :
+                        <div></div>}
                     {this.state.showMobileError ?
                         <div id="error2" className="alert alert-danger" role="alert" >
                             You must enter at least one phone number
@@ -175,6 +215,38 @@ class CreateUser extends Component {
                         :
                         <div></div>}
                 </form>
+                {/*<div>
+                    <PlacesAutocomplete
+                        value={this.state.address}
+                        onChange={this.onChangeAddress}
+                        onSelect={this.handleSelect}
+                    >
+                        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                            <div>
+                                <p>Latitude: {this.state.coordinates.lat}</p>
+                                <p>Longitude: {this.state.coordinates.lng}</p>
+
+                                <input {...getInputProps({ placeholder: "Type address" })} />
+
+                                <div>
+                                    {loading ? <div>...loading</div> : null}
+
+                                    {suggestions.map(suggestion => {
+                                        const style = {
+                                            backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
+                                        };
+
+                                        return (
+                                            <div {...getSuggestionItemProps(suggestion, { style })}>
+                                                {suggestion.description}
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                        )}
+                    </PlacesAutocomplete>
+                </div>*/}
             </div>
         );
     }
